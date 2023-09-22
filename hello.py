8ac1cd48f3c4e29b77234ee456fe979ed979e6ec
KevinSadigh <79182046+KevinSadigh@users.noreply.github.com>
2023-09-22 18:09:57 -0400

    Finished rest of Activity 4
    Commiting rest of activity 4 tasks as well as screenshots as required

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, EmailField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -14,8 +14,8 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[DataRequired()])
     submit = SubmitField('Submit')
-
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -35,5 +35,12 @@ def index():
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        #check if input contains @utoronto in it 
+        if "utoronto" not in form.email.data:
+            flash('Please enter a valid UofT email address ' + form.email.data + ' is missing utoronto')
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
